59e919d7340de94483584023d63f0968cb163a37
Ana M Grijalba <happypillusa@outlook.com>
2019-08-18 19:57:42 -0400

    making a grid

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var penColour = 'black';
-    
-  function setPenColour(pen)
-  {
-    penColour = pen;
-  }
+// function that builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
+};
 
-  function setPixelColour(pixel)
-  {
-    pixel.style.backgroundColor = penColour;
-  }
+
+ 
